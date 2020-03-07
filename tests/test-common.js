0e8d8ceb3096b855f47623bc7c31978598a48ed4
Orie Steele <orie@transmute.industries>
2020-03-07 13:06:46 -0600

    Implement fingerprint

--- tests/test-common.js
+++ tests/test-common.js
@@ -3,9 +3,70 @@
  */
 'use strict';
 
+const chai = require('chai');
+const {expect} = chai;
+
 module.exports = async function({Secp256k1KeyPair}) {
 
   describe('secp256k1-key-pair', () => {
+
+    describe('Secp256k1KeyPair', ()=>{
+      it('has working constructor', async () => {
+        const x = new Secp256k1KeyPair({
+          id: undefined,
+          controller: undefined,
+          type: 'EcdsaSecp256k1VerificationKey2019',
+          privateKeyBase58: '4HvXrvNBrmN5tUCwcjVWRpQG32CtuLvZ12xVf5rv8r1F',
+          publicKeyBase58: '231cRx1fhyNzrdj9i3UseKm1ApgMwyDLbKtJJH5AacEwL'
+        });
+        x.should.be.a('object');
+        expect(x.privateKeyBase58).to.equal('4HvXrvNBrmN5tUCwcjVWRpQG32CtuLvZ12xVf5rv8r1F');
+        expect(x.publicKeyBase58).to.equal('231cRx1fhyNzrdj9i3UseKm1ApgMwyDLbKtJJH5AacEwL');
+      });
+
+      it('has static factory method from', async () => {
+        const x = await Secp256k1KeyPair.from({
+          id: undefined,
+          controller: undefined,
+          type: 'EcdsaSecp256k1VerificationKey2019',
+          privateKeyBase58: '4HvXrvNBrmN5tUCwcjVWRpQG32CtuLvZ12xVf5rv8r1F',
+          publicKeyBase58: '231cRx1fhyNzrdj9i3UseKm1ApgMwyDLbKtJJH5AacEwL'
+        });
+        x.should.be.a('object');
+        expect(x.privateKeyBase58).to.equal('4HvXrvNBrmN5tUCwcjVWRpQG32CtuLvZ12xVf5rv8r1F');
+        expect(x.publicKeyBase58).to.equal('231cRx1fhyNzrdj9i3UseKm1ApgMwyDLbKtJJH5AacEwL');
+      });
+
+      it('has static method fingerprintFromPublicKey', async () => {
+        const f0 = Secp256k1KeyPair.fingerprintFromPublicKey({publicKeyBase58: '231cRx1fhyNzrdj9i3UseKm1ApgMwyDLbKtJJH5AacEwL'});
+        expect(f0).to.equal('zQ3shnxmSoA9BJ2Djspq8RZkh9MNcUSYvFmP8Fp46aQqhpio4')
+      });
+
+      it('has instance method fingerprintFromPublicKey', async () => {
+        const x = new Secp256k1KeyPair({
+          id: undefined,
+          controller: undefined,
+          type: 'EcdsaSecp256k1VerificationKey2019',
+          privateKeyBase58: '4HvXrvNBrmN5tUCwcjVWRpQG32CtuLvZ12xVf5rv8r1F',
+          publicKeyBase58: '231cRx1fhyNzrdj9i3UseKm1ApgMwyDLbKtJJH5AacEwL'
+        });
+        const f0 = x.fingerprint();
+        expect(f0).to.equal('zQ3shnxmSoA9BJ2Djspq8RZkh9MNcUSYvFmP8Fp46aQqhpio4')
+      });
+
+      it('has instance method verifyFingerprint', async () => {
+        const x = new Secp256k1KeyPair({
+          id: undefined,
+          controller: undefined,
+          type: 'EcdsaSecp256k1VerificationKey2019',
+          privateKeyBase58: '4HvXrvNBrmN5tUCwcjVWRpQG32CtuLvZ12xVf5rv8r1F',
+          publicKeyBase58: '231cRx1fhyNzrdj9i3UseKm1ApgMwyDLbKtJJH5AacEwL'
+        });
+        const v0 = x.verifyFingerprint('zQ3shnxmSoA9BJ2Djspq8RZkh9MNcUSYvFmP8Fp46aQqhpio4');
+        expect(v0.valid).to.equal(true)
+      });
+    })
+    
     it('properly signs and verifies', async () => {
       const x = await Secp256k1KeyPair.generate();
       const {sign} = x.signer();
