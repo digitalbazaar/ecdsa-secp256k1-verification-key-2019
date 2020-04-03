d54c8acf27c6b6a47768b81b545a97baf348b373
David I. Lehn <dil@lehn.org>
2020-04-03 14:55:53 -0400

    Remove Node.js 6 support.

--- tests/test.js
+++ tests/test.js
@@ -3,19 +3,6 @@
  */
 'use strict';
 
-// support async/await tests in node6
-if(!require('semver').gte(process.version, '8.6.0')) {
-  require('@babel/register')({
-    presets: [
-      [
-        '@babel/preset-env', {
-          targets: 'node 6'
-        }
-      ]
-    ]
-  });
-}
-
 const chai = require('chai');
 const common = require('./test-common');
 const jsigs = require('jsonld-signatures');
